e99012034d8d38d3239b6206bfd2a7533f9707fb
Alex Radu <cooldudev.super@gmail.com>
2026-03-03 14:07:18 -0500

    feat: add (public) route group layout with PublicHeader and PublicFooter

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,19 @@
+// src/app/(public)/layout.tsx
+import { PublicHeader } from "@/components/layout/PublicHeader"
+import { PublicFooter } from "@/components/layout/PublicFooter"
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <PublicHeader />
+      <main className="flex flex-col flex-1">
+        {children}
+      </main>
+      <PublicFooter />
+    </div>
+  )
+}
